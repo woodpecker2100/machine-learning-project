aa630b14f6d121e53f15de01bf87770e9e6db02d
Berta Devenyi <berta.j.devenyi@gmail.com>
2022-10-13 17:44:14 +1100

    adding files

---
+++ app.py
@@ -0,0 +1,17 @@
+
+import os
+
+from flask import (
+    Flask,
+    render_template)
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def compare():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
